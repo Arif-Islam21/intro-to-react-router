acd6979becade3ecc66e2c87ec50781441952705
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-03-20 14:59:04 +0600

    react nested routing

--- src/main.jsx
+++ src/main.jsx
@@ -3,19 +3,13 @@ import ReactDOM from "react-dom/client";
 // import App from "./App.jsx";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// import Home from "./components/home/Home";
+// import Header from "./components/header/Header";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <div>Hello from react router</div>,
-  },
-  {
-    path: "/about",
-    element: <div>I am in the about page</div>,
-  },
-  {
-    path: "/contact",
-    element: <div>call me right now</div>,
   },
 ]);
 
