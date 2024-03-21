09999a159006dd2cd81a3ee9795dfedea0848dd1
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-03-21 22:59:57 +0600

    params

--- src/components/PostDetails/PostDetails.jsx
+++ src/components/PostDetails/PostDetails.jsx
@@ -1,7 +1,8 @@
-import { useLoaderData, useNavigate } from "react-router-dom";
+import { useLoaderData, useNavigate, useParams } from "react-router-dom";
 
 const PostDetails = () => {
   const post = useLoaderData();
+  const { postId } = useParams();
   const navigate = useNavigate();
   const { id, title, body } = post;
 
